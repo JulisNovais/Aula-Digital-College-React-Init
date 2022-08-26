28678cb79f74a9bf6f3f836fe6e81c62a8c51f38
Juliete <100291387+JulisNovais@users.noreply.github.com>
2022-08-25 21:00:29 -0300

    add novo projeto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+   <div>
+    <h1>Livros</h1>
+    <div>
+      <ul>
+        <li>Memorias de uma Gueixa</li>
+        <li> O prícipe</li>
+        <li> Simarilion </li>
+      </ul>
+    </div>
+    </div>
+  );
+}
+
+export default App;
